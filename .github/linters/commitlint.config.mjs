d9663f38b5b4ce86af390091e67b456a9939d5c8
Kostis Papazafeiropoulos <papazof@gmail.com>
2025-05-02 23:38:50 +0300

    ci: Add workflow to remotely update external repos
    Add a CI workflow to remotely update submodules in `external_repos` and
    improve handling for private repos:
    - Add workflow that can be triggered from an external repo and extend
      the building workflow to enable update of submodules in
      `external_repos`. Any repo with a submodule can now trigger a remote
      build for a new revision and, optionally, create/update a PR with any
      changes.
    - Update workflows to be able to fetch private submodules
    
    PR: https://github.com/nubificus/vaccel-docs/pull/23
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

---
+++ .github/linters/commitlint.config.mjs
@@ -0,0 +1,14 @@
+export default {
+	extends: ['@commitlint/config-conventional'],
+	helpUrl: 'https://www.conventionalcommits.org/',
+	ignores: [
+		(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg),
+		(msg) => /Signed-off-by: github-actions\[bot]/m.test(msg),
+	],
+	rules: {
+		'header-max-length': [2, 'always', 72],
+		'body-max-line-length': [2, 'always', 80],
+		'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+		'trailer-exists': [2, 'always', 'Signed-off-by:'],
+	},
+}
